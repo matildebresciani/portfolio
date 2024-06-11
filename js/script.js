d550f42e79545c7f27134f9bb64d442f572a1da5
matilde <mabr0005@stud.kea.dk>
2024-06-11 14:54:46 +0200

    sidste ændringer

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-// Få den aktuelle URL
 const currentUrl = window.location.href;
 
-// Få alle links i menuen
 const menuLinks = document.querySelectorAll('header li a');
 
-// Loop gennem hvert link
 menuLinks.forEach(link => {
-  // Tjek om linkets href matcher den aktuelle URL
   if (link.href === currentUrl) {
-    // Tilføj aktiv klasse
     link.classList.add('active');
   }
 });
+
